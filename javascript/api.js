b75eb3f2fdf397fa54034c34192d97f408377a21
hnamitha1 <namitha.h@gmail.com>
2016-07-25 22:49:07 +0100

    submitting files

---
+++ javascript/api.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("#getMessage").on("click", function () {
+    var html = "";
+    $.getJSON('http://quotes.stormconsultancy.co.uk/random.json', function (json) {
+      html += json.quote + "<br>"; 
+      html += "<div class ='author'>" + json.author;
+      html += "</div>";
+      $(".message").html(html);
+      $("#tweet-link").attr("href", "https://twitter.com/intent/tweet?text=" + json.quote + " ");
+    });
+  });
+});
+
