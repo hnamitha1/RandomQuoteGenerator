987ad1dc11608ce1de5bed6ac1d5d5b5017498e3
hnamitha1 <namitha.h@gmail.com>
2016-07-29 21:29:43 +0100

    added footer

--- javascript/api.js
+++ javascript/api.js
@@ -1,13 +1,12 @@
 $(document).ready(function () {
   $("#getMessage").on("click", function () {
-    var html = "";
-    $.getJSON('http://quotes.stormconsultancy.co.uk/random.json', function (json) {
-      html += json.quote + "<br>"; 
+    var html = " ";
+		$.getJSON("https://crossorigin.me/http://quotes.stormconsultancy.co.uk/random.json", function (json) {
+      html += json.quote + "<br>";
       html += "<div class ='author'>" + json.author;
       html += "</div>";
-      $(".message").html(html);
+      $(".message").html(html);
       $("#tweet-link").attr("href", "https://twitter.com/intent/tweet?text=" + json.quote + " ");
     });
   });
 });
-
